870d70259e655aab520604acb1344e06017b1f20
Johnkm09 <johnkm09@gmail.com>
2026-05-26 21:53:03 +0300

    Settin Up

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V1\RoomController;
+use App\Http\Controllers\Api\V1\Rooms\RoomController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
